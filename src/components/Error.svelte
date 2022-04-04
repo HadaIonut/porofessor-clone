75062c3b2ac61d3f9975a1ffc7aeee93b6525abc
ionut <hadaionut@gmail.com>
2022-04-04 23:56:19 +0300

    Added generic search errors

---
+++ src/components/Error.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let imageURL;
+  export let altImg;
+  export let primaryText;
+  export let secondaryText;
+</script>
+
+<img
+    src="{imageURL}"
+    alt="{altImg}"
+    class="error-image">
+<div class="error-message">
+  <div class="primary-error">
+    {primaryText}
+  </div>
+  <div class="secondary-error">
+    {secondaryText}
+  </div>
+</div>
+
+<style lang="scss">
+  .error-image {
+    height: 20rem;
+    width: 20rem;
+  }
+
+  .primary-error {
+    color: white;
+    font-size: 24px;
+  }
+
+  .secondary-error {
+    color: #ababab;
+    font-size: 20px;
+  }
+</style>
--- src/components/SearchError.svelte
+++ src/components/SearchError.svelte
@@ -1,24 +1,31 @@
 <script>
   import backendUrl from "../constants/backendUrl.js";
+  import Error from "./Error.svelte";
 
   export let errorType;
 </script>
 
 <div class="error-container">
-  {#if (errorType = 'not-found')}
-      <img
-          src="{backendUrl}/static/errorPictures/tired-poro.png"
-          alt="Just a hurt poro :("
-          class="error-image">
-      <div class="error-message">
-        <div class="primary-error">
-          The specific user you searched for was not found.
-        </div>
-        <div class="secondary-error">
-          Your either miss-typed their name or they are not in a game
-        </div>
-      </div>
+  {#if (errorType === 'not-found')}
+    <Error
+        imageURL="{backendUrl}/static/errorPictures/tired-poro.png"
+        altImg="Just a hurt poro :("
+        primaryText="The specific user you searched for was not found."
+        secondaryText="Your either miss-typed their name or they are not in a game"/>
+  {:else if (errorType === 'unauth' || errorType === 'bad-key')}
+    <Error
+        imageURL="{backendUrl}/static/errorPictures/bad-dev-key.png"
+        altImg="Adventure poro :)"
+        primaryText="Something is wrong with your dev key"
+        secondaryText="Your dev key is either expired or wrong"/>
+  {:else }
+    <Error
+        imageURL="{backendUrl}/static/errorPictures/other-error-poro.png"
+        altImg="Cute poro"
+        primaryText="Who know what could be wrong here"
+        secondaryText="No, really, what did you do, I cannot even replicate this"/>
   {/if}
+
 </div>
 
 <style lang="scss">
@@ -29,14 +36,17 @@
     align-items: center;
     flex-direction: column;
   }
+
   .error-image {
     height: 20rem;
     width: 20rem;
   }
+
   .primary-error {
     color: white;
     font-size: 24px;
   }
+
   .secondary-error {
     color: #ababab;
     font-size: 20px;
